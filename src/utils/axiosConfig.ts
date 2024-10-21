0ea283df5c5ba840e1d5d1c86274ee3d4e898663
Mohammad Shahid Beigh <85876937+MohammadShahidBeigh@users.noreply.github.com>
2024-10-21 16:34:39 +0530

    cors upset

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://43.205.10.7:4000", // Updated to the backend URL
+  baseURL: "http://43.205.10.7:4000/graphql", // Updated to the backend URL
   headers: {
     "Content-Type": "application/json",
   },
